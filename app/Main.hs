8b8c1ec0edc2e3af9716383e0a341ca0df579059
Ryo Katsuse <ryokatsu0719@gmail.com>
2026-01-17 15:12:29 +0900

    feat: 入力としてコマンドを決めて渡せるようにする

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,25 @@
 module Main (main) where
 
+import System.Environment (getArgs)
+
 main :: IO ()
-main = putStrLn "Hello, Dev Launcher!"
+main = do
+    args <- getArgs
+    run args
+
+run :: [String] -> IO ()
+run []             = putStrLn "Usage: dev-launcher <command>"
+run ["list"]       = putStrLn "Listing projects... (not implemented yet)"
+run ["add", name]  = putStrLn $ "Adding project: " ++ name
+run ["help"]       = printHelp
+run args           = putStrLn $ "Unknown command: " ++ unwords args
+
+printHelp :: IO ()
+printHelp = putStrLn $ unlines
+    [ "dev-launcher - Development Environment Launcher"
+    , ""
+    , "Commands:"
+    , "  list        List registered projects"
+    , "  add <name>  Add a new project"
+    , "  help        Show this help"
+    ]
